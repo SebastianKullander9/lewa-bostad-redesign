40e10145993c5be9f321bbfc8230b5d2b112bdf0
SebastianKullander9 <sebastian.kullander@protonmail.com>
2026-05-22 21:41:56 +0200

    add intro section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/sections/hero/page";
+import Intro from "@/components/sections/intro/Intro";
 
 export default function Home() {
     return (
         <>
             <Hero />
+            <Intro />
         </>
     );
 }
