204622056260e851ad4ffb61b8c4473155a21cd4
SebastianKullander9 <sebastian.kullander@protonmail.com>
2026-05-22 20:43:33 +0200

    setup fonts, design tokesn, create hero section

---
+++ components/sections/hero/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import hero from "@/public/hero/hero.webp";
+
+export default function Hero() {
+    return (
+        <section className="h-screen w-full relative">
+            <Image
+                src={hero}
+                alt="hero image, showing a living room from one of lewas estate projects"
+                fill
+                className="object-cover"
+            />
+        </section>
+    );
+}
