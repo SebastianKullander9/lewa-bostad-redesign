0b1fc207a51de174899690634a6f22ab48e13a8a
SebastianKullander9 <sebastian.kullander@protonmail.com>
2026-05-25 21:14:03 +0200

    add mobile menu with slide-in animation, backdrop blur, and scroll lock

--- components/ui/hamburger/Hamburger.tsx
+++ components/ui/hamburger/Hamburger.tsx
@@ -1,32 +1,17 @@
-"use client";
-
-import { useState } from "react";
 import React from "react";
 
-interface HeaderProps {
-    children: React.ReactNode;
+interface HamburgerProps {
+    setState: (isOpen: boolean) => void;
 }
 
-export default function Hamburger({ children }: HeaderProps) {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const handleOpen = () => {
-        setIsOpen(!isOpen);
-    };
-
+export default function Hamburger({ setState }: HamburgerProps) {
     return (
         <>
-            <button onClick={handleOpen} type="button" className="flex flex-col gap-1">
+            <button onClick={() => setState(true)} type="button" className="flex flex-col gap-1">
                 <span className="w-5.5 h-0.5 bg-primary" />
                 <span className="w-5.5 h-0.5 bg-primary" />
                 <span className="w-5.5 h-0.5 bg-primary" />
             </button>
-
-            <div
-                className={`fixed z-9999 inset-0 bg-surface ${isOpen ? "opacity-100" : "opacity-0 pointer-events-none"}`}
-            >
-                {children}
-            </div>
         </>
     );
 }
--- components/ui/header/Header.tsx
+++ components/ui/header/Header.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 import logo from "@/public/logo/logo_green.png";
 import Image from "next/image";
 import { menu } from "./menu";
@@ -6,34 +9,42 @@ import Hamburger from "../hamburger/Hamburger";
 import MobileMenu from "./MobileMenu";
 
 export default function Header() {
-    return (
-        <header className="fixed w-full base-x-p py-sm z-100 section-surface" role="banner">
-            <div className="flex flex-row justify-between items-center">
-                <Link href="/" aria-label="Lewa Bostad - Till startsidan">
-                    <Image
-                        src={logo}
-                        alt="The logo for the company lewa bostad"
-                        className="h-7 md:h-11 w-auto"
-                        priority
-                    />
-                </Link>
+    const [isOpen, setIsOpen] = useState(false);
 
-                <nav
-                    className="hidden md:flex  flex-row gap-3xl text-body"
-                    aria-label="Huvudnavigation"
-                >
-                    {menu.map((item, index) => (
-                        <Link key={index} href={item.href} className="link-hover-fade">
-                            {item.label}
-                        </Link>
-                    ))}
-                </nav>
-                <div className="md:hidden">
-                    <Hamburger>
-                        <MobileMenu />
-                    </Hamburger>
+    return (
+        <>
+            <header className="fixed w-full base-x-p base-y-p z-100 section-surface" role="banner">
+                <div className="flex flex-row justify-between items-center">
+                    <Link href="/" aria-label="Lewa Bostad - Till startsidan">
+                        <Image
+                            src={logo}
+                            alt="The logo for the company lewa bostad"
+                            className="h-7 md:h-11 w-auto"
+                            priority
+                        />
+                    </Link>
+
+                    <nav
+                        className="hidden md:flex  flex-row gap-3xl text-body"
+                        aria-label="Huvudnavigation"
+                    >
+                        {menu.map((item, index) => (
+                            <Link key={index} href={item.href} className="link-hover-fade">
+                                {item.label}
+                            </Link>
+                        ))}
+                    </nav>
+                    <div className="md:hidden">
+                        <Hamburger setState={setIsOpen} />
+                    </div>
                 </div>
-            </div>
-        </header>
+            </header>
+
+            <MobileMenu isOpen={isOpen} setIsOpen={setIsOpen} />
+
+            <div
+                className={`${isOpen ? "opacity-100" : "opacity-0"} fixed inset-0 bg-black/10 backdrop-blur-xs z-9998 pointer-events-none transition-opacity duration-200`}
+            ></div>
+        </>
     );
 }
